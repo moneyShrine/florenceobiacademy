0a36c8a9a6f606d91857afe307624285e0d6a449
moneyshrine <joeyekpe@gmail.com>
2025-04-03 10:38:42 +0100

    Some corrections made

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,16 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'custom-bg': "url('/build/img/bg-img.png')",
-        'mini-img-1': "url('/build/img/std_1.png')",
-        'mini-img-2': "url('/build/img/std_2.png')",
-        'mini-img-3': "url('/build/img/std_3.png')",
-        'mini-img-4': "url('/build/img/std_4.png')",
-        'creche-img': "url('/build/img/creche.png')",
-        'nursery-img': "url('/build/img/nursery.png')",
-        'primary-img': "url('/build/img/primary.png')",
-        'secondary-img': "url('/build/img/secondary.png')",
-        'qoute-img': "url('/build/img/qoute_img.jpg')",
+        'custom-bg': "url('/assets/img/bg-img.png')",
+        'mini-img-1': "url('/assets/img/std_1.png')",
+        'mini-img-2': "url('/assets/img/std_2.png')",
+        'mini-img-3': "url('/assets/img/std_3.png')",
+        'mini-img-4': "url('/assets/img/std_4.png')",
+        'creche-img': "url('/assets/img/creche.png')",
+        'nursery-img': "url('/assets/img/nursery.png')",
+        'primary-img': "url('/assets/img/primary.png')",
+        'secondary-img': "url('/assets/img/secondary.png')",
+        'qoute-img': "url('/assets/img/qoute_img.jpg')",
       },
       backgroundSize: {
         'height-contain': '100% contain', // Width 100%, height contain
